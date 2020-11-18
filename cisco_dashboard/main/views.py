1e01266c4a4767c0d1ad04fc5c59da401cd19394
Fraser Dale <2387625d@student.gla.ac.uk>
2020-11-18 15:37:55 +0000

    API Calls made every request not pop script
    As specified by cisco, api calls should be constantly made to reflect
    api calls. This was made easy due to the preexisting population
    script. I simply functioned it and implemented it into views.

--- cisco_dashboard/main/views.py
+++ cisco_dashboard/main/views.py
@@ -42,6 +42,7 @@ def index(request):
     
 
 def overview(request):
+    updateAll('6bec40cf957de430a6f1f2baa056b99a4fac9ea0')
     context_dict = {
         'allOrgs':  Organisation.objects.all(),
     }
@@ -116,6 +117,94 @@ def user_logout(request):
     logout(request)
     return redirect(reverse('index'))
 
+def updateAll(apikey):
+    dash = meraki.DashboardAPI(apikey)
+    updateOrganizations(dash)
+    for org in dash.organizations.getOrganizations():
+        updateNetwork(dash,org['id'])
+        #for net in dash.organizations.getOrganizationNetworks(org['id']):
+        #    updateDevices(dash,net['id'])
 
-#def ruofan(request):
-#    return HttpResponse("Ruofan's page...")
+def updateOrganizations(dash):
+    GETorgs = dash.organizations.getOrganizations() #Get all organizations
+
+    for org in GETorgs:
+
+        try:
+            Organisation.objects.get(orgID=org['id'])
+            org_to_update = Organisation.objects.filter(orgID=org['id'])
+            org_to_update.update(
+                orgID   = org['id'],
+                orgName = org['name'],
+                orgURL  = org['url'],
+                #orgAPIOverview = APIOverview
+            )
+        except:
+            newOrg = Organisation.objects.create(
+                orgID   = org['id'],
+                orgName = org['name'],
+                orgURL  = org['url'],
+                #orgAPIOverview = APIOverview
+            )
+            newOrg.save()
+    
+
+
+#updates all networks for an organization ID
+def updateNetwork(dash,orgID):
+    GETnets = dash.organizations.getOrganizationNetworks(orgID)
+    newOrg = Organisation.objects.get(orgID=orgID)
+    for net in GETnets:
+        try:
+            Network.objects.get(netID = net['id'])
+            net_to_update = Network.objects.filter(netID = net['id'])
+            net_to_update.update(
+                    org     = newOrg,
+                    netID   = net['id'],
+                    netName = net['name']
+            )
+        except:
+            newNet = Network.objects.create(
+                    org     = newOrg,
+                    netID   = net['id'],
+                    netName = net['name']
+            )
+            newNet.save()
+
+#updates all devices for a network ID
+def updateDevices(dash,netID):
+    GETdevs = dash.networks.getNetworkDevices(netID)
+    newNet= Network.objects.get(netID=netID)
+            
+    for device in GETdevs:
+        try:
+            Device.objects.get(devSerial = device['serial'])
+            dev_to_update = Device.objects.filter(devSerial = device['serial'])
+            dev_to_update.update(
+                net = newNet, 
+                        
+                devAddr   = device['address'],
+                        
+                devSerial = device['serial'],
+                devMac    = device['mac'],
+                devModel  = device['model'],
+                        #devLanIP  = device['lanIp'],
+                        
+                devLat    = device['lat'],
+                devLong   = device['lng']
+                )
+        except:
+            newDevice = Device.objects.create(
+                net = newNet,
+
+                devAddr   = device['address'],
+                        
+                devSerial = device['serial'],
+                devMac    = device['mac'],
+                devModel  = device['model'],
+                        #devLanIP  = device['lanIp'],
+                        
+                devLat    = device['lat'],
+                devLong   = device['lng']
+                )
+            newDevice.save()
