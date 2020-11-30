14b2a7bf4834753ab69989e83b39cdeb762e6e29
Jake Haakanson <2407682h@student.gla.ac.uk>
2020-11-30 12:52:35 +0000

    Update forms.py inline with pylint #close 30
    Added module docstring
    Added docstrings to individual classes
    Removed whitespace on newlines

--- cisco_dashboard/main/forms.py
+++ cisco_dashboard/main/forms.py
@@ -1,15 +1,31 @@
+"""Django forms file
+ *  - UserForm          - Used during signup to collect user information
+ *  - UserProfileForm   - Used to update or collect user API key information
+"""
+
+
 from django import forms
 from django.contrib.auth.models import User
 from main.models import *
 
 
 class UserForm(forms.ModelForm):
+    """UserForm form
+     *  - username  - User's username  
+     *  - email     - User's email
+     *  - password  - User's password
+    """
     password = forms.CharField(widget=forms.PasswordInput())
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password',)
 
+
 class UserProfileForm(forms.ModelForm):
+    """UserProfileForm form
+     *  - apikey    - User's API key
+    """
 	class Meta:
 		model = UserProfile
 		fields = ('apikey',)
