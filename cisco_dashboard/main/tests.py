154f5ef70efce5fc686cf56199c6e4fe5e0834ce
Ruofan Guo <2431011g@student.gla.ac.uk>
2020-12-01 07:59:55 +0000

    Update tests.py

--- cisco_dashboard/main/tests.py
+++ cisco_dashboard/main/tests.py
@@ -1,19 +1,21 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 #https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Testing
-# Create your tests here.
 
+# Create your tests here.
 class register(TestCase):
     def setUp(self):
+        '''
         self.credentials = {
-            'username': 'testuser',
-            'email address':'549956326@qq.com',
-            'apikey':'customerday1',
-            'password': 'secret'}
-        User.objects.create_user(**self.credentials)
+            'username': 'testuser1',
+            'email':'549956326@qq.com',
+            'password': 'secret',
+            'apikey':'customerday1'}
+        User.objects.create_user(**self.credentials)'''
     def test_register(self):
         # send register data
-        response = self.client.post('/register/', self.credentials, follow=True)
+        #response = self.client.post('/register/', self.credentials, follow=True)
+        response = self.client.post('/register/', {'username':'aa', 'email':'549956326@qq.com','password':'secret', 'apikey':'customerday1' }, follow=True)
         # should be logged in now
         print('hello register')
 #        self.assertTrue(response.context['username'].is_active)
@@ -30,7 +32,6 @@ class LogInTest(TestCase):
         # should be logged in now
         print('hello login')
 #        self.assertTrue(response.context['username'].is_active)
-
 class YourTestClass(TestCase):
     @classmethod
     def setUpTestData(cls):
@@ -52,4 +53,3 @@ class YourTestClass(TestCase):
     def test_one_plus_one_equals_two(self):
         print("Method: test_one_plus_one_equals_two.")
         self.assertEqual(1 + 1, 2)
-
