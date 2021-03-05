814d7cfec549011fd0a8a1fe3c50197525dc1dd0
2431011g <2431011g@student.gla.ac.uk>
2021-03-05 13:53:31 +0800

    Add test for same name or email.

--- cisco_dashboard/main/tests.py
+++ cisco_dashboard/main/tests.py
@@ -4,34 +4,53 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from .models import *
 from django.contrib.auth import authenticate
-#https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Testing
+
+
+# https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Testing
 
 # Create your tests here.
 class RegisterLoginTest(TestCase):
     """Tests registration and login functionality"""
+
     def test_register_and_login(self):
         """
          * Test registration functionality by registering a test account
          * Also test login functionality by logging in to the registered user account
         """
         user_creds = {
-            'username':'aa',
-            'email':'549956326@qq.com',
-            'password':'secret',
-            'apikey':'customerday1'
+            'username': 'aa',
+            'email': '549956326@qq.com',
+            'password': 'secret',
+            'apikey': 'customerday1'
         }
 
         response = self.client.post(
             '/register/',
             user_creds,
-            follow = True
+            follow=True
         )
 
-        self.assertTrue(response.status_code == 200)    # Check registration was successful
-        assert User.objects.get(username = "aa")        # Ensure user exists
+        self.assertTrue(response.status_code == 200)  # Check registration was successful
+        assert User.objects.get(username="aa")  # Ensure user exists
 
-        user = authenticate(username='aa', password='secret')         # Login with register details
-        self.assertTrue((user is not None) and user.is_authenticated) # Assert login was successful
+        user = authenticate(username='aa', password='secret')  # Login with register details
+        self.assertTrue((user is not None) and user.is_authenticated)  # Assert login was successful
+
+
+class TestUsername(TestCase):
+    """Tests username """
+    def test_username(self):
+        user_name = 'Ruofan'
+        user = User.objects.get(username=user_name)
+        assert user
+
+
+class TestEmail(TestCase):
+    """Tests user email"""
+    def test_email(self):
+        user_email = '2356466399@qq.com'
+        user = User.objects.get(email=user_email)
+        assert user
 
 
 class TestAPI(TestCase):
@@ -46,26 +65,25 @@ class TestAPI(TestCase):
          * This function retrieves the DevNet Sandbox organisation and stores it in the db
          * The organisation will then be retrieved and checked
         """
-        api_key = "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"    # Test API keys
-        dash = meraki.DashboardAPI(api_key) # Set up dash using API keys
+        api_key = "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"  # Test API keys
+        dash = meraki.DashboardAPI(api_key)  # Set up dash using API keys
 
         try:
             devnet = dash.organizations.getOrganization("549236")
 
             test_org = Organisation.objects.create(
-                org_id   = devnet['id'],
-                org_name = devnet['name'],
-                org_url  = devnet['url'],
-                apikey = api_key
+                org_id=devnet['id'],
+                org_name=devnet['name'],
+                org_url=devnet['url'],
+                apikey=api_key
             )
             test_org.save()
 
         except meraki.exceptions.APIError:
             assert False
 
-
         try:
-            get_org = Organisation.objects.get(org_id = "549236")
+            get_org = Organisation.objects.get(org_id="549236")
             self.assertEqual(get_org, test_org)
 
         except Organisation.DoesNotExist:
@@ -75,10 +93,10 @@ class TestAPI(TestCase):
 class ChangeAPIKeyTest(TestCase):
     def test_change_api_key(self):
         testdata = {
-            'username':'testuseralpha',
-            'email':'testuser@test.com',
-            'password':'testuser',
-            'apikey':'testcase1'
+            'username': 'testuseralpha',
+            'email': 'testuser@test.com',
+            'password': 'testuser',
+            'apikey': 'testcase1'
         }
 
         self.credentials = {
@@ -86,12 +104,13 @@ class ChangeAPIKeyTest(TestCase):
             'password': 'testuser'}
 
         self.client.post('/register/', testdata, follow=True)
-        self.client.login(username=testdata['username'],password=testdata['password'])
-        testdatabeta = {'apikey':'testcase2'}
+        self.client.login(username=testdata['username'], password=testdata['password'])
+        testdatabeta = {'apikey': 'testcase2'}
         self.client.post('/editapikey', testdatabeta, follow=True)
         user = User.objects.get(username=testdata['username'])
-        userprofile = UserProfile.objects.get(user = user)
+        userprofile = UserProfile.objects.get(user=user)
         assert (userprofile.apikey == testdatabeta['apikey'])
+
 
 '''
 class LogOutTest(TestCase):
@@ -113,19 +132,19 @@ class LogOutTest(TestCase):
         self.assertFalse(user.is_authenticated)
 '''
 
+
 class UseDemoKeyTest(TestCase):
     def test_demo_key(self):
         testprofile = {
             'username': 'testuser',
-            'email':'testuser@test.com',
+            'email': 'testuser@test.com',
             'password': 'secret',
-            'apikey':'testcase1'
-            }
+            'apikey': 'testcase1'
+        }
         self.client.post('/register/', testprofile, follow=True)
-        self.client.login(username=testprofile['username'],password=testprofile['password'])
+        self.client.login(username=testprofile['username'], password=testprofile['password'])
         req = self.client.post('/usedemokey', follow=True)
         user = User.objects.get(username=testprofile['username'])
-        userprofile = UserProfile.objects.get(user = user)
-        testdatabeta = {'apikey':'demo'}
+        userprofile = UserProfile.objects.get(user=user)
+        testdatabeta = {'apikey': 'demo'}
         self.assertEqual(userprofile.apikey, testdatabeta['apikey'])
-        
